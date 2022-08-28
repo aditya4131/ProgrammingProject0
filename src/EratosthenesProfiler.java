39777fac6ec3914a0c02ef0291489bde5b0f6268
aditya4131 <asriva8@lsu.edu>
2022-08-27 21:20:58 -0500

    Fixed the formatting issue and the changed the logic.

--- src/EratosthenesProfiler.java
+++ src/EratosthenesProfiler.java
@@ -12,23 +12,63 @@
  * </pre>
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.lang.Math;
 public class EratosthenesProfiler extends EratosthenesUtil {
     public EratosthenesProfiler() {
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter a number or enter 000 to quit: ");
-        int input = in.nextInt();
-
+        System.out.println("Enter an integer n to generate primes in [2,n] ");
+        long input = in.nextLong();
+        ArrayList<Long> result = new ArrayList<>();
 
         EratosthenesUtil a = new EratosthenesUtil();
         long startTime = System.nanoTime();
-        a.toString(sieve(input));
+        result = a.sieve(input);
         long endTime = System.nanoTime();
         long elapsedTime = endTime - startTime;
-        input = 0;
-        System.out.println("Time: " + elapsedTime / 1000000);
+        long finalTime = elapsedTime /1000;
+
+        System.out.print("P(" + input + ") = " ); System.out.print((a.toString(result)));
+        System.out.println();
+        System.out.println("Time to Generate the Primes: " + finalTime + " microseconds");
+        System.out.println("pi(" + input + ") = " + result.size());
+        System.out.println();
+
+
+        System.out.printf("%-12s %-12s %-12s %-12s %-12s \n" , "n", "Time(us)", "pi(n)", "n/ln(n)", "%Error in pi(n)");
+
+       while( input != -1) {
+            long startTime1 = System.nanoTime();
+            result = a.sieve(input);
+            long endTime1 = System.nanoTime();
+            long elapsedTime1 = endTime1 - startTime1;
+            long finalTime1 = elapsedTime1 /1000;
+
+            double log = input/Math.log(input);
+            double pi = Math.PI * input;
+            double error = ((log - pi)/pi);
+            double finalError = error * 100;
+
+
+            System.out.printf("%-12d %-12d %-12d %-12.1f %-12.1f", input, finalTime1, result.size(),log, finalError );
+            System.out.println();
+           //System.out.println("Enter an integer n to generate primes in [2,n] ");
+           input = in.nextLong();
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- src/EratosthenesUtil.java
+++ src/EratosthenesUtil.java
@@ -16,27 +16,27 @@ import java.util.Arrays;
 import java.lang.Math;
 
 class EratosthenesUtil {
-    public static ArrayList<Integer> sieve(long n) {
+    public static ArrayList<Long> sieve(long n) {
         if (n < 2) { // returns an empty arraylist if n is less than two
             return new ArrayList();
         } else {
-            ArrayList<Integer> primeNumbers = new ArrayList();
+            ArrayList<Long> primeNumbers = new ArrayList();
             boolean[] isPrimeNumber = new boolean[(int) n];
             Arrays.fill(isPrimeNumber, true);
 
             isPrimeNumber[0] = false;
             isPrimeNumber[1] = false;
 
-            for (int i = 2; i < Math.sqrt(n); i++) {
-                if (isPrimeNumber[i]) {
-                    for (int j = 2; i * j < n; j++) {
-                        isPrimeNumber[i * j] = false;
+            for (long i = 2; i < Math.sqrt(n); i++) {
+                if (isPrimeNumber[(int) i]) {
+                    for (long j = i*i;  j < n; j+=i) {
+                        isPrimeNumber[(int) j] = false;
                     }
                 }
             }
 
-            for (int i = 2; i < n; i++) {
-                if (isPrimeNumber[i]) {
+            for (long i = 2; i < n; i++) {
+                if (isPrimeNumber[(int) i]) {
                     primeNumbers.add(i);
                 }
             }
@@ -44,7 +44,7 @@ class EratosthenesUtil {
         }
     }
 
-    public static String toString(ArrayList<Integer> v) {
+    public static String toString(ArrayList<Long> v) {
         String primes = "";
 
         System.out.print("{");
@@ -56,6 +56,6 @@ class EratosthenesUtil {
         }
         System.out.print("}");
 
-        return "{" + primes + "}";
+        return primes;
     }
 }
