b59ba9f59db52bda00fd7eb0a928cbf6566c2dfe
aditya4131 <asriva8@lsu.edu>
2022-09-01 11:52:16 -0500

    Formatting changes to profiler.

--- src/EratosthenesProfiler.java
+++ src/EratosthenesProfiler.java
@@ -2,6 +2,7 @@
  * A program to profile the Eratosthene's Sieve prime generator
  * and compare the exact value of the prime-counting function and
  * the n/ln(n) approximation.
+ *
  * @author Duncan, YOUR NAME
  * @see EratosthenesUtil
  * <pre>
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.lang.Math;
+
 public class EratosthenesProfiler {
     public EratosthenesProfiler() {
     }
@@ -33,40 +35,33 @@ public class EratosthenesProfiler {
         result = EratosthenesUtil.sieve(input);
         long endTime = System.nanoTime();
         long elapsedTime = endTime - startTime;
-        long finalTime = elapsedTime /1000;
+        long finalTime = elapsedTime / 1000;
 
-       System.out.print("P(" + input + ") = " ); System.out.print((EratosthenesUtil.toString(result)));
+        System.out.print("P(" + input + ") = ");
+        System.out.print((EratosthenesUtil.toString(result)));
         System.out.println();
         System.out.println("Time to Generate the Primes: " + finalTime + " microseconds");
         System.out.println("pi(" + input + ") = " + result.size());
         System.out.println();
 
 
-        System.out.printf("%-12s %-12s %-12s %-12s %-12s \n" , "n", "Time(us)", "pi(n)", "n/ln(n)", "%Error in pi(n)");
+        System.out.printf("%-12s %-12s %-12s %-12s %-12s \n", "n", "Time(us)", "pi(n)", "n/ln(n)", "%Error in pi(n)");
 
-       for(int i = 10000; i<=150000; i+=10000) {
+        for (int i = 10000; i <= 150000; i += 10000) {
             long startTime1 = System.nanoTime();
             ArrayList<Long> finalPrimeResult = EratosthenesUtil.sieve(i);
             long endTime1 = System.nanoTime();
             long elapsedTime1 = endTime1 - startTime1;
-            long finalTime1 = elapsedTime1 /1000;
+            long finalTime1 = elapsedTime1 / 1000;
 
-            double log = i/Math.log(i);
+            double log = i / Math.log(i);
             //double pi = Math.PI * i;
             long size = finalPrimeResult.size();
-            double error = ((log - size )/ size) * 100;
+            double error = ((log - size) / size) * 100;
 
-            System.out.printf("%-12d %-12d %-12d %-12.1f %-12.1f", i, finalTime1, size,log, error );
+            System.out.printf("%-12d %-12d %-12d %-12.1f %-12.1f", i, finalTime1, size, log, error);
             System.out.println();
         }
-
-
-
-
-
-
-
-
 
 
     }
